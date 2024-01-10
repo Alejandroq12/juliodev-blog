ee8666df1e74476eebde721fa9f40fa0c0c08872
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-10 15:54:17 -0600

    Feat: run migrations with complete table Posts.

--- db/migrate/20240110210648_create_posts.rb
+++ db/migrate/20240110210648_create_posts.rb
@@ -1,8 +1,15 @@
 class CreatePosts < ActiveRecord::Migration[7.1]
   def change
     create_table :posts do |t|
+      t.integer :author_id, null: false, index: true
+      t.string :title
+      t.text :text
+      t.integer :comments_counter, default: 0
+      t.integer :likes_counter, default: 0
 
       t.timestamps
     end
+
+    add_foreign_key :posts, :users, column: author_id
   end
 end
