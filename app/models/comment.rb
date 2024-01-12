48f68ab49fb11829c3ac5d097ad5feab5053212c
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-12 16:22:20 -0600

    Feat: create custom method that updates the comments counter for a post.

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :post
+
+  after_create :update_comment_counter
+
+  private
+
+  def update_comment_counter
+    post.increment!(:comments_counter)
+  end
 end
