4a1ec87c733b7783a5e717a7b3196e6de30f8b9d
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-12 16:25:23 -0600

    Feat: create custom method that returns the 3 most recent posts for a given user.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ApplicationRecord
   has_many :posts, foreign_key: :author_id
   has_many :comments
   has_many :likes
+
+  def self.recent_posts(user_id)
+    find(user_id).posts.order(created_at: :desc).limit(3)
+  end
 end
