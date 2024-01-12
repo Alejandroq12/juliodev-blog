eec9725817f2d1a0fbe015facf99dc62340bfee6
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-12 16:22:56 -0600

    Feat: create custom method that updates the likes counter for a post.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,12 @@
 class Like < ApplicationRecord
   belongs_to :post
   belongs_to :user
+
+  after_create :update_likes_counter_post
+
+  private
+
+  def update_likes_counter_post
+    post.increment!(:likes_counter)
+  end
 end
