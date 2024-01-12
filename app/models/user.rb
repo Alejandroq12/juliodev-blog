a1ee6b5938ecdc932edecbf0bbf5acd25d3ff29b
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-12 12:43:11 -0600

    Fix: relationships in User Model.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many :posts, class_name: 'Post', foreign_key: :author_id
+  has_many :posts, foreign_key: :author_id
   has_many :comments
   has_many :likes
 end
