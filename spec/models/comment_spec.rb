23f0a352b793a10637cf283911ca0db18d6ba746
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-13 17:41:05 -0600

    Test: add RSpec tests for Comment Model.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:first_user) { User.create(name: 'Julio', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Developer from El salvador.') }
+  let(:first_post) { Post.create(author: first_user, title: 'Hello', text: 'This is my first post') }
+
+  describe '#update_comments_counter' do
+    it 'increments the comments counter of the post' do
+      expect { Comment.create(user: first_user, post: first_post) }.to change {
+                                                                         first_post.reload.comments_counter
+                                                                       }.by(1)
+    end
+  end
 end
