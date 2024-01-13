10a7be1a002006cc9765d9efb1c28dc5e55193d6
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-13 17:40:51 -0600

    Test: add RSpec tests for Likes Model.

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:first_user) { User.create(name: 'Tom', photo: 'https://unsplash.com/photos/F_-0BxGuVvo', bio: 'Teacher from Mexico.') }
+  let(:first_post) { Post.create(author: first_user, title: 'Hello', text: 'This is my first post') }
+
+  describe '#update_likes_counter_post' do
+    it 'increments the likes counter of the post' do
+      expect { Like.create(user: first_user, post: first_post) }.to change { first_post.reload.likes_counter }.by(1)
+    end
+  end
 end
