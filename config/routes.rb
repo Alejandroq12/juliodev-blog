9c01a7aaf90aae3fa4b93499901857581d67cc55
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-16 11:06:22 -0600

    Feat: set routes for users and posts.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,13 @@
 Rails.application.routes.draw do
+  resources :users, only: [:index, :show] do 
+    resources :posts, only: [:index, :show]
+  end
+
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'users/index'
+  # get 'users/show'
+  root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
