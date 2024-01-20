07441b31dc01b961c37351e66ecd2895c55af021
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-19 21:11:46 -0600

    Refactor user controller to display view correctly.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,10 @@
 class UsersController < ApplicationController
   def index
-    @users = User.all
+    @users = User.includes(:posts).all
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.includes(:posts).find(params[:id])
+    @posts = @user.posts
   end
 end
