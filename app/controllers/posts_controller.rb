a19cd89cec396750c425c91c73bafbc16652924e
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-19 21:12:07 -0600

    Refactor: post controller to display view correctly.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   def index
-    @posts = Post.all
+    @user = User.find(params[:user_id])
+    @posts = @user.posts.includes(:author, :comments, :likes)
   end
 
   def show
