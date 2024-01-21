90c0db3e0835281c95a95f748c75f4e0b17aed8b
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-21 15:18:56 -0600

    Refactor: add new and create methods..

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :set_user, only: %i[index new create]
+
   def index
     @user = User.find(params[:user_id])
     @posts = @user.posts.includes(:author, :comments, :likes)
@@ -6,6 +8,32 @@ class PostsController < ApplicationController
 
   def show
     @post = Post.find(params[:id])
+    @user = current_user
     @recent_comments = Post.recent_comments(@post.id)
+    @comment = Comment.new
+    @like = Like.new
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = @user.posts.build(post_params)
+
+    respond_to do |format|
+      if @post.save
+        format.html { redirect_to user_posts_path(user_id: @user.id), notice: 'Post was successfylly saved' }
+      else
+        format.turbo_stream
+        format.html { render :new }
+      end
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :text)
   end
 end
