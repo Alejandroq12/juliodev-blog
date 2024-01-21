f968aab6f3c158a66fb59fe3e3ab17185ec41480
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-21 13:43:49 -0600

    Feat: add current user method.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+  helper_method :current_user # Make current_user available as a helper method in the views
+
+  private
+
+  def current_user
+    # This is just a stub and it will be replaced with actual
+    # authentication logic in future milestones.
+    @current_user = User.first
+  end
 end
