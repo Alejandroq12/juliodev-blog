55945a04da73da8a5d19e2e537a894cbd22fc044
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-21 15:15:47 -0600

    Feat: add routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: [:index, :show] do 
-    resources :posts, only: [:index, :show]
+  resources :users do 
+    resources :posts do
+      resources :likes, only: [:create]
+      resources :comments, only: [:new, :create]
+    end
   end
+  
 
   root 'users#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
