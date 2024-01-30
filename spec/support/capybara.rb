76f98fbb7b606b8e03f75e5c54dc5285014bfc3e
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-30 15:28:30 -0600

    Test: add support files.

---
+++ spec/support/capybara.rb
@@ -0,0 +1,10 @@
+require 'capybara/rspec'
+
+RSpec.configure do |config|
+  config.before(:each, type: :system) do
+    driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+  end
+  # config.before(:each, type: :system) do
+  #   driven_by :selenium, using: :headless_chrome, screen_size: [1400, 1400]
+  # end
+end
