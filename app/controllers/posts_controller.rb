b5523eb3251cbe61febe428d92da068eef8aad6a
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-01-30 15:48:15 -0600

    Feat: add pagination to the post controller.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @posts = @user.posts.order(created_at: :desc)
+    @posts = @user.posts.order(created_at: :desc).paginate(page: params[:page], per_page: 4)
     @posts.each(&:recent_comments)
   end
 
