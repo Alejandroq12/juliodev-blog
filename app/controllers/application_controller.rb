3406837a8349e40f0dc61d293077ca654196b26c
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-03 17:27:45 -0600

    Feat: permit parameters to register and update a user.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,15 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
   before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[name photo bio email password])
+    devise_parameter_sanitizer.permit(:account_update, keys: %i[name photo bio email password])
+  end
+
+  def set_user
+    @user = current_user
   end
 end
