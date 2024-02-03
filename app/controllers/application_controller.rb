e7d57e83fb186acb24641f4ff0220168b501c74b
Juan Carlos Muñoz Navarrete <jcmunav63@gmail.com>
2024-02-03 05:52:25 -0600

    Update application_controller.rb

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,9 @@
 class ApplicationController < ActionController::Base
-  helper_method :current_user # Make current_user available as a helper method in the views
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
   private
 
-  def current_user
-    # This is just a stub and it will be replaced with actual
-    # authentication logic in future milestones.
-    @current_user = User.first
-  end
-
-  def set_user
-    @user = current_user
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
   end
 end
