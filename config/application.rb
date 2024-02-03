f46f7c29707e69aa4b0b10baccf0f502ac68d41c
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-02-03 17:25:27 -0600

    Feat: add basic devise configuration.

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module JuliodevBlog
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w(assets tasks))
+    config.autoload_paths << Rails.root.join('lib')
+
 
     # Configuration for the application, engines, and railties goes here.
     #
